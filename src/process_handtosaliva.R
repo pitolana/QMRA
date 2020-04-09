409efa55402aa1c7609e89654c45c43c3b2bc98a
Ana Karina Pitol <50717201+pitolana@users.noreply.github.com>
2020-04-09 11:12:23 +0100

    Second commit. Adding paper, references, and first draft of the process for analysing hand to saliva data.

---
+++ src/process_handtosaliva.R
@@ -0,0 +1,13 @@
+
+# Processing data of virus transfer from hands to saliva
+
+# Data taken from Pitol, 2017. Available at <https://zenodo.org/record/1454505#.Xo3wMZNKjBI>
+
+# dt = drying time for average droplet size (min), assumtion. 
+
+dt= 10
+
+TE_hm = m * TE_hm_d + (1-m) * TE_hm_w
+if t = 0, m=0,
+if 
+
